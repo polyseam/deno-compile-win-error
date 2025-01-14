64b56b8e655c2a8ebb306f41daeb435ed7415a37
Matt Johnston <m@ttjohnston.com>
2025-01-14 04:31:44 -0400

    homedir + async

--- src/deno-compile-win-error.ts
+++ src/deno-compile-win-error.ts
@@ -1,26 +1,12 @@
-import { ensureDirSync } from "@std/fs";
+// import { ensureDirSync } from "@std/fs";
 import { homedir } from "node:os";
-import * as path from "@std/path";
+// import * as path from "@std/path";
 import { delay } from "@std/async";
 
 export async function denoCompileWinError() {
   await delay(1);
   const home = homedir();
-  const whereTheProblemsAre = path.join(home, "problematic");
-
-  const w = new Worker(import.meta.resolve("src/worker.ts"), {
-    type: "module",
-  });
-
-  try {
-    ensureDirSync(whereTheProblemsAre);
-
-    throw new Error(
-      "this will not be logged if compiled\nfor windows on deno v2.1.5",
-    );
-  } catch (err) {
-    console.error(err as Error);
+  if (home) {
+    console.log(`This will not be printed when compiled for windows`);
   }
-
-  w.postMessage("worker I am trapped in a computer please elp");
 }
