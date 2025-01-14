1ee8cd9071c94364617092cfca6dacad0a8c43f7
Matt Johnston <m@ttjohnston.com>
2025-01-14 03:37:21 -0400

    as Error; gitignore

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  const message =
-    "this will not be logged if compiled\nfor windows on deno v2.1.5";
-  console.log(message as string);
+  try {
+    throw new Error("this will not be logged if compiled\nfor windows on deno v2.1.5");
+  } catch (err){
+    console.error(err as Error);
+  }
 }
